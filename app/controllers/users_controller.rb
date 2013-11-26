b32ca72030663968ff0408086cf686a509e49814
Onni Hakala <hakala.onni@gmail.com>
2013-11-27 00:06:10 +0200

    Finish of user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)     #Not final implementation
+    @user = User.new(user_params)
     if @user.save
       # Handle the succesful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -18,7 +20,7 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params # Getting rid of some extra parameters that attackers are using
+  def user_params # Getting rid of some extra parameters that attackers may be using
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,14 +31,14 @@ describe "UserPages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name", with: "Example User"
-        fill_in "Email", with: "user@example"
-        fill_in "Password", with: "foobar"
-        fill_in "Password", with: "foobar"
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
       end
 
       it "should create a user" do
-        expect {click_button submit}.to change(User, :count).by(1)
+        expect { click_button submit }.to change(User, :count).by(1)
       end
     end
   end
